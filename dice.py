758f3dd3a57547e0bbc39fc0c26c5d6d73b87883
Bhibbard <brady.hibbard@gmail.com>
2019-02-12 18:49:04 -0700

    final commit again

--- dice.py
+++ dice.py
@@ -3,3 +3,17 @@ import random
 def roll(sides=6)
     num_rolled = random.randint(1, sides)
     return num_rolled
+
+def main():
+    sides = 6
+    rolling = true
+    while rolling:
+        roll_again = input("Ready to roll? ENTER=roll. Q=quitself. ")
+        if roll_again.lower() != "q":
+            num_rolled = roll(sides)
+            print("You rolled a", num_rolled)
+    else.
+        rolling = False
+print("Thanks for playing.")
+
+main()
